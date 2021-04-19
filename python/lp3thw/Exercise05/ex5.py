0ca0039bd496025e7a53a7d4cc264b69bed4bb30
Ganome <ganome@gmail.com>
2021-04-19 17:06:12 +0000

    Exercises 4-12 lp3thw

---
+++ python/lp3thw/Exercise05/ex5.py
@@ -0,0 +1,18 @@
+myName = "Ganome"
+myAge = 35
+myHeight = 72
+myWeight = 180
+myEyes = "Blue"
+myTeeth = "white"
+myHair = "Brown"
+
+print(f"Let's talk about {myName}.")
+print(f"He's {myHeight} inches tall.")
+print(f"He's {myWeight} pounds heavy.")
+print(f"Actually that's not too heavy.")
+print(f"Hes got {myEyes} eyes and {myHair} hair.")
+print(f"His teeth are usuallyy {myTeeth} depending on the coffee.")
+
+#tricky line
+total = myAge + myHeight + myWeight
+print(f"If i add {myAge}, {myHeight}, and {myWeight} I get {total}.")
---
+++ python/lp3thw/Exercise10/ex10.py
@@ -0,0 +1,15 @@
+tabbyCat = "\tI'm Tabbed in."
+persianCat = "I'm Split\non a line."
+backslashCat = "I'm \\ a \\ cat."
+
+fatCat = """
+I'll do a list:
+\t* Cat Food
+\t* Fishies
+\t* Catnit\n\t* Grass
+"""
+
+print(tabbyCat)
+print(persianCat)
+print(backslashCat)
+print(fatCat)
