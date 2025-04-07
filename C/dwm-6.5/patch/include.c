be36380d32efd030081cc52d5cb678adb4a43442
ganome <ganome@gmail.com>
2025-04-07 12:35:27 -0600

    Added a copy of DWM-6.5 - UNMAINTAINED.  See parent Repo for updates!

---
+++ C/dwm-6.5/patch/include.c
@@ -0,0 +1,42 @@
+/* Bar functionality */
+#include "bar_indicators.c"
+#include "bar_tagicons.c"
+#include "bar.c"
+
+#include "bar_ewmhtags.c"
+#include "bar_ltsymbol.c"
+#include "bar_status.c"
+#include "bar_tabgroups.c"
+#include "bar_tagpreview.c"
+#include "bar_tags.c"
+#include "bar_wintitle.c"
+#include "bar_systray.c"
+#include "bar_wintitleactions.c"
+
+/* Other patches */
+#include "alttab.c"
+#include "attachx.c"
+#include "cfacts.c"
+#include "cool_autostart.c"
+#include "cyclelayouts.c"
+#include "dragcfact.c"
+#include "ipc.c"
+#ifdef VERSION
+#include "ipc/IPCClient.c"
+#include "ipc/yajl_dumps.c"
+#include "ipc/ipc.c"
+#include "ipc/util.c"
+#endif
+#include "movestack.c"
+#include "pertag.c"
+#include "restartsig.c"
+#include "renamed_scratchpads.c"
+#include "swallow.c"
+#include "dragmfact.c"
+/* Layouts */
+#include "layout_facts.c"
+#include "layout_fibonacci.c"
+#include "layout_grid.c"
+#include "layout_monocle.c"
+#include "layout_tile.c"
+
---
+++ C/dwm-6.5/patch/include.h
@@ -0,0 +1,37 @@
+/* Bar functionality */
+#include "bar_indicators.h"
+#include "bar_tagicons.h"
+#include "bar.h"
+
+#include "bar_ewmhtags.h"
+#include "bar_ltsymbol.h"
+#include "bar_status.h"
+#include "bar_tabgroups.h"
+#include "bar_tags.h"
+#include "bar_tagpreview.h"
+#include "bar_wintitle.h"
+#include "bar_systray.h"
+#include "bar_wintitleactions.h"
+
+/* Other patches */
+#include "alttab.h"
+#include "attachx.h"
+#include "cfacts.h"
+#include "cool_autostart.h"
+#include "cyclelayouts.h"
+#include "dragcfact.h"
+#include "dragmfact.h"
+#include "ipc.h"
+#include "ipc/ipc.h"
+#include "ipc/util.h"
+#include "movestack.h"
+#include "pertag.h"
+#include "restartsig.h"
+#include "renamed_scratchpads.h"
+#include "swallow.h"
+/* Layouts */
+#include "layout_fibonacci.h"
+#include "layout_grid.h"
+#include "layout_monocle.h"
+#include "layout_tile.h"
+
